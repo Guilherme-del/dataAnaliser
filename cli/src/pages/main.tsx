e4adaba0a2e32c7514dcf2e877202e3fb5a959d3
Guilherme Cavenaghi <guilherme_cavenaghi@hotmail.com>
2022-08-15 15:14:13 -0300

    1.0.1

--- cli/src/pages/main.tsx
+++ cli/src/pages/main.tsx
@@ -8,10 +8,11 @@ import * as config from "../config/config"
 import { Doughnut } from 'react-chartjs-2';
 import AddPeople from '../components/Addpeople'
 import { getPeople, addPeople, deletePeople } from '../controller/API'
+import { delay } from '../controller/mainFunc';
 //styles
 import "antd/dist/antd.css";
 import 'react-toastify/dist/ReactToastify.css';
-import { Main,PrimaryText,SecondaryText,DataInfo,TableStyle, ChartComponent } from '../assets/styles/styled';
+import { Main, PrimaryText, SecondaryText, DataInfo, TableStyle, ChartComponent } from '../assets/styles/styled';
 
 
 const App: React.FC = () => {
@@ -22,19 +23,18 @@ const App: React.FC = () => {
   ChartJS.register(ArcElement, Tooltip, Legend);
 
   useEffect(() => {
-    if(people.length <= 0 ){
-      fetchPeople()  
+    if (people.length <= 0) {
+      fetchPeople()
     }
-    else if (labelChart.length <= 0 || dataChart.length <= 0){
+    else if (labelChart.length <= 0 || dataChart.length <= 0) {
       manipulatedDataChart()
     }
     else {
-      console.log(infoChart,dataChart)
       return
     }
   }, [people])
 
-  //chart config variable
+  //chart configuration variable
   const infoChart = {
     labels: labelChart ? labelChart : ['NoData'],
     datasets: [
@@ -42,7 +42,7 @@ const App: React.FC = () => {
         data: dataChart ? dataChart : [0],
         backgroundColor: config.default.colours,
         borderColor: config.default.colours,
-        borderWidth:1,
+        borderWidth: 1,
       },
     ],
   };
@@ -54,18 +54,20 @@ const App: React.FC = () => {
     const dataChart = people.map((items) => {
       return (items.participation)
     })
-      console.log(labelChart)
-      setLabelChart(labelChart)
-      setDataChart(dataChart)
-  }
-
-  async function delay(ms: number) {
-    await new Promise<void>(resolve => setTimeout(() => resolve(), ms));
+    setLabelChart(labelChart)
+    setDataChart(dataChart)
   }
 
   async function fetchPeople() {
     getPeople().then(({ data: { people } }) => { setPeople(people) })
-      .catch((err: Error) => console.log(err))
+      .catch((err: Error) => toast.error(`${err}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true
+      }))
   }
 
   const handleSavePeople = (e: React.FormEvent, formData: IPeople): void => {
@@ -90,7 +92,7 @@ const App: React.FC = () => {
           });
         }
         else {
-          toast.error('Erro,tente novamente!', {
+          toast.error('Erro! não foi possivel concluir ação.', {
             position: "top-right",
             autoClose: 5000,
             hideProgressBar: true,
@@ -99,23 +101,29 @@ const App: React.FC = () => {
             draggable: true
           });
         }
-
       })
-      .catch((err) => console.log(err))
+      .catch((err) => toast.error(`${err}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true
+      }))
   }
 
-  //Attributes to EditPeople
-  //  const handleUpdatePeople = (people: IPeople): void => {
-  //    updatePeople(people)
-  //      .then(({ status, data }) => {
-  //        if (status !== 200) {
-  //          throw new Error('Error! Todo not updated')
-  //        }
-  //        setPeople(data.people)
-  //      })
-  //      .catch((err) => console.log(err))
-  //  }
-  //
+  /* Attributes to Edit Person
+  const handleUpdatePeople = (people: IPeople): void => {
+    updatePeople(people)
+      .then(({ status, data }) => {
+        if (status !== 200) {
+          throw new Error('Error! People not updated')
+        }
+        setPeople(data.people)
+      })
+        .catch((err) => console.log(err))
+   }
+  */
 
   const handleDeletePeople = (e: any, record: any): void => {
     e.preventDefault()
@@ -139,7 +147,7 @@ const App: React.FC = () => {
           });
         }
         else {
-          toast.error('Erro,tente novamente!', {
+          toast.error('Erro! não foi possivel concluir ação.', {
             position: "top-right",
             autoClose: 5000,
             hideProgressBar: true,
@@ -149,29 +157,34 @@ const App: React.FC = () => {
           });
         }
       })
-      .catch((err) => console.log(err))
+      .catch((err) => toast.error(`${err}`, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: true,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true
+      }))
   }
 
   return (
     <Main className='App'>
-      <>
-        <AddPeople savePeople={handleSavePeople} />
-        <PrimaryText>DATA</PrimaryText>
-        <SecondaryText>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</SecondaryText>
-        <DataInfo>
-          <TableStyle>
-            <Table rowKey='lastName' dataSource={people ? people : []} pagination={{ pageSize: 5 }}>
-              <Column title="First Name" dataIndex="firstName" key="firstName" />
-              <Column title="Last Name" dataIndex="lastName" key="lastName" />
-              <Column title="Participation" dataIndex="participation" key="participation" />
-              <Column title="Remove" key="btns" render={(_text, record, _index) => <Button onClick={(e) => { handleDeletePeople(e, record) }} icon={<DeleteOutlined />} />} />
-            </Table>
-          </TableStyle>
-          <ChartComponent>
-            <Doughnut data={infoChart} />
-          </ChartComponent>
-        </DataInfo>
-      </>
+      <AddPeople savePeople={handleSavePeople} />
+      <PrimaryText>DATA</PrimaryText>
+      <SecondaryText>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</SecondaryText>
+      <DataInfo>
+        <TableStyle>
+          <Table rowKey='lastName' dataSource={people ? people : []} pagination={{ pageSize: 5 }}>
+            <Column title="First Name" dataIndex="firstName" key="firstName" />
+            <Column title="Last Name" dataIndex="lastName" key="lastName" />
+            <Column title="Participation" dataIndex="participation" key="participation" />
+            <Column title="Remove" key="btns" render={(_text, record, _index) => <Button onClick={(e) => { handleDeletePeople(e, record) }} icon={<DeleteOutlined />} />} />
+          </Table>
+        </TableStyle>
+        <ChartComponent>
+          <Doughnut data={infoChart} />
+        </ChartComponent>
+      </DataInfo>
       <ToastContainer
         position="top-right"
         autoClose={5000}
